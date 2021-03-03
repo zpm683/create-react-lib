9b008bd20a2c426c17cad3e5a6647963f31d2fb3
下午茶溢栈 <zpm683@qq.com>
2021-03-03 22:06:11 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,41 @@
  */
 
 const del = require("del");
-const { renameSync, readdirSync } = require("fs");
+const path = require("path");
 const { execSync } = require("child_process");
 const { series, dest, src } = require("gulp");
+const editJsonFile = require("edit-json-file");
+const { renameSync, readdirSync } = require("fs");
 
 const getNeedToBuildDirs = () => {
   const DO_NOT_BUILD = ["setupTests.ts", "react-app-env.d.ts", "typings.d.ts"];
   const all = readdirSync("./src");
   return [...all].filter((x) => [...DO_NOT_BUILD].every((y) => y !== x));
+};
+
+const autoSetPublishConfig = (keysToMerge, autoUpgrade = false) => {
+  const packageJson = require("./package.json");
+  const publishConfigPath = path.resolve(__dirname, "./.publish/package.json");
+  const publishConfig = editJsonFile(publishConfigPath);
+
+  for (let key of keysToMerge) {
+    const value = packageJson[key];
+    if (value !== undefined) {
+      publishConfig.set(key, value);
+    }
+  }
+
+  if (autoUpgrade) {
+    const [hV, mV, lV] = publishConfig.get("version").split(".");
+    if (!hV | !mV | !lV) {
+      console.error("bad version in .publish/package.json");
+    } else {
+      const nextVer = `${hV}.${Number(mV) + 1}.${lV}`;
+      publishConfig.set("version", nextVer);
+    }
+  }
+
+  publishConfig.save();
 };
 
 const DO_BUILD_DIRS = getNeedToBuildDirs();
@@ -43,6 +70,9 @@ const afterBuild = () => {
      // FIXBUG https://github.com/developit/microbundle/pull/799/files
     del.sync(`dist/${dirname}.js.map`);
   }
+  // auto merge package.json
+  const MERGE_KEYS = ["peerDependencies", "dependencies"];
+  autoSetPublishConfig(MERGE_KEYS, true);
   // copy some files
   return src([".publish/*"]).pipe(dest("dist/"));
 };
